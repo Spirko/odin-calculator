4b5bf78814546b6468328d5cd99ecd0d2322aed9
Spirko <spirko@gmail.com>
2022-08-03 23:16:01 -0500

    Encapsulated myBuffer a little better.

--- mybuffer.js
+++ mybuffer.js
@@ -1,6 +1,6 @@
 class myBuffer {
-  buffer = [];
-  resolvers = [];
+  #buffer = [];
+  #resolvers = [];
 
   // No need for a constructor.
 
@@ -8,18 +8,24 @@ class myBuffer {
   //   but wait for a putItem if the buffer is empty.
   getItem() {
     let p = new Promise((resolve,reject) => {
-      let curItem = this.buffer.shift();
-      if (curItem) resolve(curItem);
-      else this.resolvers.push(resolve);
+      this.#resolvers.push(resolve);
+      this.resolvePromises();
     });
     return p;
+  }
+
+  resolvePromises() {
+    while (this.#resolvers.length && this.#buffer.length) this.#resolvers.shift()(this.#buffer.shift());
   }
 
   // putItem will add an item to the buffer.
   // If there is a waiting promise, resolve it.
   putItem(newItem) {
-    this.buffer.push(newItem);
-    while (this.resolvers.length && this.buffer.length) this.resolvers.shift()(this.buffer.shift());
-    this.resolve = undefined;
+    this.#buffer.push(newItem);
+    this.resolvePromises();
+  }
+
+  showBuffer() {
+    return this.#buffer;
   }
 }
